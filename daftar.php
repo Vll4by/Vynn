8e6fe0217c009ec8e393a9060553709cdc9c1e8e
Vynn13 <dalghiff29@gmail.com>
2021-04-09 03:11:10 +0700

    3:10

--- daftar.php
+++ daftar.php
@@ -30,8 +30,6 @@
           <div class="col-12 col-sm-10 offset-sm-1 col-md-8 offset-md-2 col-lg-8 offset-lg-2 col-xl-8 offset-xl-2">
             <div class="login-brand">
 
-              <img src="assets2/img/avatar/avatar-2.png" alt="logo" width="100" >
-
               <img src="assets2/img/avatar/avatar-2.PNG" alt="logo" width="100" >
 
             </div>
